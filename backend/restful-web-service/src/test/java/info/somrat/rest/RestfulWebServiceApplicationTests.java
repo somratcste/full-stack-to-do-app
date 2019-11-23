5795740a1f4608f0b530a5944d6cb96f1eac19e4
somratcste <somratcste@gmail.com>
2019-11-23 19:08:13 +0600

    initialize backend spring boot application

---
+++ backend/restful-web-service/src/test/java/info/somrat/rest/RestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package info.somrat.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

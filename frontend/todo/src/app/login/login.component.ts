34a1ecf46fe399797b7387c49488bfd2226b3cf1
somratcste <somratcste@gmail.com>
2019-11-23 16:17:52 +0600

    configure routing

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = 'nazmul'
+  password = ''
+  errorMessage = 'Invalid Credentials'
+  invalidLogin = false
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleLogin() {
+    if (this.username === 'nazmul' && this.password === 'password') {
+      this.invalidLogin = true;
+    }
   }
 
 }

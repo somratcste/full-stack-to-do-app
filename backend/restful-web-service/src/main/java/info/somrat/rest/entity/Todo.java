6c89fe3e0a7f32c6556e408459ae151a1810cb63
somratcste <somratcste@gmail.com>
2019-11-24 01:11:45 +0600

    todos id auto increment

--- backend/restful-web-service/src/main/java/info/somrat/rest/entity/Todo.java
+++ backend/restful-web-service/src/main/java/info/somrat/rest/entity/Todo.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 public class Todo {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column

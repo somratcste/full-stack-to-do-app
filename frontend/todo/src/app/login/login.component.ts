1af748a74f067b5d1b03ef8b61c367f0a402be1b
somratcste <somratcste@gmail.com>
2019-11-23 16:25:27 +0600

    dummy login and navigate

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,13 +13,14 @@ export class LoginComponent implements OnInit {
   errorMessage = 'Invalid Credentials'
   invalidLogin = false
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
   handleLogin() {
     if (this.username === 'nazmul' && this.password === 'password') {
+      this.router.navigate(['welcome']);
       this.invalidLogin = true;
     }
   }
